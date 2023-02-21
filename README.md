b18d743de511b404ba596310170cbb9ba77ebd74
Ravana ⭐ <47528708+ravana69@users.noreply.github.com>
2023-02-21 16:30:06 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Halloween 2022
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/ricardoolivaalonso/pen/RwyqraN](https://codepen.io/ricardoolivaalonso/pen/RwyqraN).
+
---
+++ src/script.js
@@ -0,0 +1,18 @@
+/*
+	Designed by: Hash Elias
+	Original image: https://dribbble.com/shots/5425482-LOST
+*/
+
+const startTrigger = document.querySelector("#start")
+const a = document.querySelector("#a")
+
+const startAnimation = () => {
+	startTrigger.classList.add("is-start-visible")
+	a.loop = true;
+	a.play()
+	startTrigger.removeEventListener("click", startAnimation)
+	setTimeout(() => startTrigger.classList.add("is-start-hidden"), 2001)
+};
+
+startTrigger.addEventListener("click", startAnimation)
+
